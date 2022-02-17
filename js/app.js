8a06d4471b5dba1e86ccedbd145510f317c445f0
AnikaLabiba <anikalabibajahin@gmail.com>
2022-02-17 22:26:44 +0600

    saving part added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 // adding eventlistener on Calculate button
 document.getElementById('calculate-btn').addEventListener('click', function () {
+    const incomeInput = document.getElementById('income-input').value;
+    // const incomeAmount = parseFloat(incomeInput);
+    const expense = calculateTotalExpenses();
 
-    calculateTotalExpenses();
-    calculateTotalBalance()
+
+    if (expense > incomeInput) {
+        notifyErrorAlert('insuficiantBalance');
+    }
+    else {
+        calculateTotalBalance();
+        closeErrorAlert('insuficiantBalance');
+    }
+
+
 })
 //calculating expenses
 function calculateTotalExpenses() {
@@ -17,14 +28,14 @@ function calculateTotalExpenses() {
         //calculating total expenses
         const expense = parseFloat(foodInput) + parseFloat(clothesInput) + parseFloat(rentInput);
         totalExpenses.innerText = expense;
-        closeErrorAlert()
+        closeErrorAlert('forPositive');
+
         return expense;
     }
-    else {
-        notifyNegativeError();
-
-
+    else if (foodInput == '' || clothesInput == '' || rentInput == '') {
+        notifyErrorAlert('forPositive');
     }
+
 }
 
 //calculating total balance
@@ -34,25 +45,42 @@ function calculateTotalBalance() {
     const incomeAmount = parseFloat(incomeInput);
     const totalBalance = document.getElementById('total-balance');
 
-    const calculateBtn = document.getElementById('calculate-btn');
     //getting expenses value
     const expenseTotal = calculateTotalExpenses();
 
     // cheching if income amount is valid or not
-    if (incomeAmount < 0 || typeof (expenseTotal) != 'number') {
-        notifyNegativeError();
+    if (incomeAmount < 0 && typeof (expenseTotal) != 'number') {
+        notifyErrorAlert('forPositive');
     }
     else {
         //calculating total balance
-        const balance = parseFloat(incomeAmount) - expenseTotal;
+        const balance = incomeAmount - expenseTotal;
         totalBalance.innerText = balance;
-        closeErrorAlert()
+        closeErrorAlert('forPositive');
         return balance;
 
     }
 }
+//eventListener on save button
+document.getElementById('save-btn').addEventListener('click', function () {
+
+    const remainingBalance = document.getElementById('remaining-balance');
+
+    const saving = calculateSavings();
+    const balance = calculateTotalBalance();
+
+    if (saving > balance) {
+        notifyErrorAlert('insuficiantBalance');
+
+    }
+    else {
+        remainingBalance.innerText = balance - saving;
+        closeErrorAlert('insuficiantBalance');
+    }
+})
 //calculating savings
 function calculateSavings() {
+
     //getting income value
     const incomeInput = document.getElementById('income-input').value;
     const incomeValue = parseFloat(incomeInput);
@@ -60,35 +88,40 @@ function calculateSavings() {
     const savingInput = document.getElementById('saving-input').value;
     const savingValue = parseFloat(savingInput);
     const savingAmount = document.getElementById('saving-amount');
-    const remainingBalance = document.getElementById('remaining-balance');
 
-    //cgeking if inputs are positive
-    if (incomeValue > 0 && savingValue > 0) {
-        const saving = (savingValue / 100) * incomeValue
+
+
+    //cheking if inputs are positive
+    if (savingValue > 0) {
+        const saving = (savingValue / 100) * incomeValue;
         savingAmount.innerText = saving;
-
-        const balance = calculateTotalBalance();
-        remainingBalance.innerText = balance - saving;
-        closeErrorAlert()
+        closeErrorAlert('forPositive');
         return saving;
     }
     else {
-        notifyNegativeError();
+        notifyErrorAlert('forPositive');
+
     }
+
 }
+
+
 
 // Error handling js//------->
 
 // handling negative number as input
-function notifyNegativeError() {
-    const notifyNegative = document.getElementById('notify-negative');
-    notifyNegative.style.display = 'block';
+function notifyErrorAlert(error) {
+    const errorAlert = document.getElementById('notify-' + error);
+    errorAlert.style.display = 'block';
 }
 //hiding error notification
-function closeErrorAlert() {
-    const notifyNegative = document.getElementById('notify-negative');
-    notifyNegative.style.display = 'none';
+function closeErrorAlert(error) {
+    const errorAlert = document.getElementById('notify-' + error);
+    errorAlert.style.display = 'none';
 }
+
+
+
 
 
 
