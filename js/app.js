288e86098c8d638cf704a9ffc6a48a7568315a5d
AnikaLabiba <anikalabibajahin@gmail.com>
2022-02-17 10:18:52 +0600

    expense and balance calculation added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log('connedted');
+
+// adding eventlistener on Calculate button
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    //getting input fields
+    const incomeInput = document.getElementById('income-input').value;
+
+    const foodInput = document.getElementById('food-input').value;
+    const clothesInput = document.getElementById('clothes-input').value;
+    const rentInput = document.getElementById('rent-input').value;
+
+
+    //getting expense and balance innerText
+    const totalExpenses = document.getElementById('total-expenses');
+
+    const totalBalance = document.getElementById('total-balance');
+
+    //calculating total expenses
+    const expense = parseFloat(foodInput) + parseFloat(clothesInput) + parseFloat(rentInput);
+    totalExpenses.innerText = expense;
+
+    //calculating total balance
+    const balance = parseFloat(incomeInput) - expense;
+    totalBalance.innerText = balance;
+
+
+
+})
