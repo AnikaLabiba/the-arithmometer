c11c905275c7d70f3efa026214f52037c76390a9
AnikaLabiba <anikalabibajahin@gmail.com>
2022-02-17 12:12:43 +0600

    saving calculation added

--- js/app.js
+++ js/app.js
@@ -1,27 +1,55 @@
 
 // adding eventlistener on Calculate button
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    //getting input fields
-    const incomeInput = document.getElementById('income-input').value;
 
+    calculateTotalExpenses();
+    calculateTotalBalance()
+})
+//calculating expenses
+function calculateTotalExpenses() {
     const foodInput = document.getElementById('food-input').value;
     const clothesInput = document.getElementById('clothes-input').value;
     const rentInput = document.getElementById('rent-input').value;
 
-
-    //getting expense and balance innerText
     const totalExpenses = document.getElementById('total-expenses');
-
-    const totalBalance = document.getElementById('total-balance');
 
     //calculating total expenses
     const expense = parseFloat(foodInput) + parseFloat(clothesInput) + parseFloat(rentInput);
     totalExpenses.innerText = expense;
+    return expense;
+
+}
+
+//calculating total balance
+function calculateTotalBalance() {
+    //getting input fields
+    const incomeInput = document.getElementById('income-input').value;
+    const totalBalance = document.getElementById('total-balance');
+
+    //getting expenses value
+    const expenseTotal = calculateTotalExpenses();
 
     //calculating total balance
-    const balance = parseFloat(incomeInput) - expense;
+    const balance = parseFloat(incomeInput) - expenseTotal;
     totalBalance.innerText = balance;
+    return balance;
 
+}
+//calculating savings
+function calculateSavings() {
+    //getting income value
+    const incomeInput = document.getElementById('income-input').value;
+    const incomeValue = parseFloat(incomeInput);
+    //getting  saving input and remainging balance 
+    const savingInput = document.getElementById('saving-input').value;
+    const savingValue = parseFloat(savingInput);
+    const savingAmount = document.getElementById('saving-amount');
+    const remainingBalance = document.getElementById('remaining-balance');
 
+    const saving = (savingValue / 100) * incomeValue
+    savingAmount.innerText = saving;
 
-})
+    const balance = calculateTotalBalance();
+    remainingBalance.innerText = balance - saving;
+}
+
